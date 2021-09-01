3e889c31de5af3111aa6519989487314adb75e82
TechnicalFungi <89542231+TechnicalFungi@users.noreply.github.com>
2021-09-01 11:11:42 -0400

    Update Dial_In_Project.py

--- SemesterProject/Dial_In_Project.py
+++ SemesterProject/Dial_In_Project.py
@@ -6,46 +6,29 @@ from kivymd.app import MDApp
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.snackbar import Snackbar
 
-KV = '''
-MDBoxLayout:
-    orientation: "vertical"
 
-    MDToolbar:
-        title: "Menu"
-        left_action_items: [["menu", lambda x: app.callback(x)]]
-        right_action_items: [["dots-vertical", lambda x: app.callback(x)]]
+BUT_1 = '''
+MDScreen:
 
-    MDLabel:
-        text: "DialIn"
-        halign: "center"
+    MDRoundFlatButton:
+        text: "Press Here"
+        text_color: 0, 1, 0, 1
+        pos_hint: {"center_x": 0.5, "center_y": 0.5}
+
 '''
 
-
-class Test(MDApp):
+class app(MDApp):
     def build(self):
         Window.size = (375,812)
-        menu_items = [
-            {
-                "viewclass": "OneLineListItem",
-                "text": f"Item {i}",
-                "height": dp(50),
-                "on_release": lambda x=f"Item {i}": self.menu_callback(x),
-             } for i in range(5)
-        ]
-        self.menu = MDDropdownMenu(
-            items=menu_items,
-            width_mult=1.5,
-        )
-        return Builder.load_string(KV)
 
-    def callback(self, button):
-        self.menu.caller = button
-        self.menu.open()
-
-    def menu_callback(self, text_item):
-        self.menu.dismiss()
-        Snackbar(text=text_item).open()
+        return Builder.load_string(BUT_1)
 
 
-Test().run()
-print("hello world")
+
+
+app().run()
+
+
+
+
+
